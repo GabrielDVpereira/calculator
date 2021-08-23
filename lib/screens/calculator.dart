f529550e8b7b9130718008aebbfba48912a4411d
gabrieldvpereira <gdavi.gd@gmail.com>
2021-08-23 11:31:02 -0300

    feat: finish button

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,8 +1,12 @@
 import 'package:calculator/components/display.dart';
+import 'package:calculator/components/keyboard.dart';
 import 'package:flutter/material.dart';
 
 class Calculator extends StatelessWidget {
   const Calculator({Key? key}) : super(key: key);
+  _onPressed(String text) {
+    print(text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +14,7 @@ class Calculator extends StatelessWidget {
       home: Column(
         children: [
           Display(text: '1234'),
-          Text('Keyboard'),
+          Keyboard(cb: _onPressed),
         ],
       ),
     );
